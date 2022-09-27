e7034a548b14836b6414540377ea80be37f5eb3a
monahajc <70150648+monahajc@users.noreply.github.com>
2022-09-26 21:10:25 -0400

    Road Block
    Didn't touch library code, but getting error saying a piece is missing

---
+++ PlayerMovementAndColors/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public Rigidbody bullet;
+    private float bulletSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
